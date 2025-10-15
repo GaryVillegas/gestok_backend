b96c3e62fb7855b9e0cadd511c43914a3e97ea17
GaryVillegas <gary.villegasb@gmail.com>
2025-10-15 12:30:17 -0300

    TODO: create a good DB model 4 this

---
+++ models/Users.py
@@ -0,0 +1,13 @@
+class Users:
+
+    def __init__(self, id, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'password': self.password
+        }
